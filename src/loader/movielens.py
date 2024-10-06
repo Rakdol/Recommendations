1d66dc728f4ab8dba9255535d8f5f0c43cff3f15
Rakdol <seongmun91@gmail.com>
2024-10-07 03:49:52 +0900

    update movielens loader

--- src/loader/movielens.py
+++ src/loader/movielens.py
@@ -1,16 +1,29 @@
 import os
+from typing import List, Dict
 from datetime import datetime
-
+from dataclasses import dataclass
 import pandas as pd
 import numpy as np
 from uszipcode import SearchEngine
+
+
+@dataclass(frozen=True)
+class MovieLensDataset(object):
+    train: pd.DataFrame
+    test: pd.DataFrame
+
+    test_user2item: Dict[int, List[int]]
+
+    user_content: pd.DataFrame
+    item_content: pd.DataFrame
+    rating_content: pd.DataFrame
 
 
 class MovieLensLoader(object):
 
     def __init__(
         self,
-        num_users: int = 1000,
+        num_users: int = 500,
         num_test_items: int = 5,
         data_path: str = "../data/ml-100k/",
     ):
@@ -38,6 +51,27 @@ class MovieLensLoader(object):
             "war",
             "western",
         ]
+
+    def load(self) -> MovieLensDataset:
+
+        ratings, moveis, users, rating_user_movies = self._load_dataset()
+        movielens_train, movielens_test = self._split_data(rating_user_movies)
+        # Test For ranking
+        movielens_test_user2items = (
+            movielens_test[movielens_test["rating"] >= 4]
+            .groupby("user_id")
+            .agg({"movie_id": list})["movie_id"]
+            .to_dict()
+        )
+
+        return MovieLensDataset(
+            movielens_train,
+            movielens_test,
+            movielens_test_user2items,
+            moveis,
+            users,
+            ratings,
+        )
 
     def _load_dataset(self):
         dateparse = lambda x: datetime.utcfromtimestamp(int(x)).strftime(
@@ -100,7 +134,26 @@ class MovieLensLoader(object):
         users = self._convert_zip_code(users=users)
         users = self._add_age_group(users=users)
 
-        return ratings, movies, users
+        # user 수를 num_users로 줄인다
+        valid_user_ids = sorted(ratings.user_id.unique())[: self.num_users]
+        ratings = ratings[ratings.user_id <= max(valid_user_ids)]
+        users = users[users.user_id <= max(valid_user_ids)]
+
+        raitng_users = pd.merge(ratings, users, on="user_id", how="inner")
+        rating_users_movies = pd.merge(raitng_users, movies, on="movie_id", how="inner")
+
+        return ratings, movies, users, rating_users_movies
+
+    def _split_data(self, movielens: pd.DataFrame):
+
+        movielens["rating_order"] = movielens.groupby("user_id")["timestamp"].rank(
+            ascending=False, method="first"
+        )  # Rank Order for watched movies, rank 1 means that latest watched movie.
+
+        movielens_train = movielens[movielens["rating_order"] > self.num_test_items]
+        movielens_test = movielens[movielens["rating_order"] <= self.num_test_items]
+
+        return movielens_train, movielens_test
 
     def _add_age_group(self, users: pd.DataFrame) -> pd.DataFrame:
         users["age_group"] = users.age.apply(
@@ -131,5 +184,6 @@ class MovieLensLoader(object):
                 city.append(np.nan)
 
         users["city"] = city
+        users.city.fillna(value="unknown", inplace=True)
 
         return users
